330426c7fac9ebc5956665d571466c04d86ce9d8
Brad Azevedo <brad2k@gmail.com>
2026-04-09 17:10:54 -0700

    V1

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,34 +9,20 @@ export default defineConfig({
 
   fonts: [
     {
-      name: "Lora",
-      cssVariable: "--font-Lora",
-      provider: fontProviders.google(),
-    },
-    {
       name: "Fraunces",
       cssVariable: "--font-Fraunces",
       provider: fontProviders.google(),
-      weights: [400, 500, 600, 700],
+      weights: [400, 600, 700],
       styles: ["normal", "italic"],
       subsets: ["latin"],
       fallbacks: ["serif"],
     },
     {
-      name: "DM Sans",
-      cssVariable: "--font-DM",
-      provider: fontProviders.google(),
-      weights: [400, 700, 800, 900],
-      styles: ["normal"],
-      subsets: ["latin"],
-      fallbacks: ["sans-serif"],
-    },
-    {
       name: "Epilogue",
       cssVariable: "--font-Epilogue",
       provider: fontProviders.google(),
-      weights: [400, 600, 700, 900],
-      styles: ["normal", "italic"],
+      weights: [400, 600, 700],
+      styles: ["normal"],
       subsets: ["latin"],
       fallbacks: ["sans-serif"],
     },
